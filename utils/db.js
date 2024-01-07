aabb1fe0acc1c77d7a5794dd374c75512a97e54b
Taka005 <01taka005@gmail.com>
2024-01-07 12:45:57 +0900

    change

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,24 @@
+const fs = require("fs");
+
 module.exports = {
   status:{
-    add:()=>{
-      
+    add:(cpu,memory)=>{
+      const data = JSON.parse(fs.readFileSync("./database/status.json","utf8"));
+
+      data.push({
+        time: new Date(),
+        cpu: cpu,
+        memory: memory
+      });
+
+      fs.writeFileSync("./database/status.json",JSON.stringify(data),"utf8");
+    },
+    remove:()=>{
+      const data = JSON.parse(fs.readFileSync("./database/status.json","utf8"));
+
+      data.shift();
+
+      fs.writeFileSync("./database/status.json",JSON.stringify(data),"utf8");
     }
-  }
+  },
 }
