be3f855a483bf906b0e7aab0272da7d89759273e
Taka005 <01taka005@gmail.com>
2024-01-09 18:39:40 +0900

    change

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,23 @@ router.get("/",(req,res)=>{
 
   if(req.session.user.name !== "admin") return res.render("admin/error");
 
-  res.render("admin/index");
+  const data = JSON.parse(fs.readFileSync("./database/account.json","utf8"));
+
+  res.render("admin/index",{
+    userCount: data.length
+  });
+});
+
+router.get("/user",(req,res)=>{
+  if(!req.session.user) return res.redirect("/account/login");
+
+  if(req.session.user.name !== "admin") return res.render("admin/error");
+
+  const data = JSON.parse(fs.readFileSync("./database/account.json","utf8"));
+
+  res.render("admin/user",{
+    users: data.map(ac=>ac.name)
+  });
 });
 
 module.exports = router;
