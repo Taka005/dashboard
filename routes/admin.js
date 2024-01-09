73a0cef08d78093075488edacf3f51923c6d02dd
Taka005 <01taka005@gmail.com>
2024-01-09 10:03:05 +0000

    fix

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const fs = require("fs");
+const Account = require("../components/Accounts");
+
+const account = new Account();
 const router = express.Router();
 
 router.get("/",(req,res)=>{
@@ -6,10 +10,10 @@ router.get("/",(req,res)=>{
 
   if(req.session.user.name !== "admin") return res.render("admin/error");
 
-  const data = JSON.parse(fs.readFileSync("./database/account.json","utf8"));
+  account.load();
 
   res.render("admin/index",{
-    userCount: data.length
+    userCount: account.data.length
   });
 });
 
@@ -18,11 +22,27 @@ router.get("/user",(req,res)=>{
 
   if(req.session.user.name !== "admin") return res.render("admin/error");
 
-  const data = JSON.parse(fs.readFileSync("./database/account.json","utf8"));
+  account.load();
 
   res.render("admin/user",{
-    users: data.map(ac=>ac.name)
+    users: account.data
   });
+});
+
+router.post("/user",(req,res)=>{
+  if(!req.session.user) return res.redirect("/account/login");
+
+  if(req.session.user.name !== "admin") return res.render("admin/error");
+
+  if(!req.body.name) return res.render("admin/user",{ errorMessage: "不正な操作です" });
+
+  if(!account.isAdmin(req.body.name)) return res.render("admin/user",{ errorMessage: "管理者は削除できません" });
+
+  account.load();
+
+  account.remove(req.body.name);
+
+  res.render("admin/user",{ successMessage: `${req.body.name}を削除しました` });
 });
 
 module.exports = router;
