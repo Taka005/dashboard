59f1e65b01f011fbd9ec4eb29e43710de67359f6
Taka005 <01taka005@gmail.com>
2024-01-10 09:28:49 +0000

    change

--- public/assets/js/userRemove.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll(".userRemove")
-  .forEach(btn=>[
-    btn.addEventListener("submit",()=>{
-      document.getElementById("removeInput").value = btn.id;
-      document.getElementById("removeForm").submit();
-    })
-  ]);
--- routes/admin.js
+++ routes/admin.js
@@ -33,17 +33,22 @@ router.post("/user",(req,res)=>{
 
   if(req.session.user.name !== "admin") return res.render("admin/error");
 
+  account.load();
+
+  res.locals.users = account.data;
+
   if(!req.body.name) return res.render("admin/user",{ errorMessage: "不正な操作です" });
 
-  if(!account.isAdmin(req.body.name)) return res.render("admin/user",{ errorMessage: "管理者は削除できません" });
-
-  account.load();
+  if(account.isAdmin(req.body.name)) return res.render("admin/user",{ errorMessage: "管理者は削除できません" });
 
   account.remove(req.body.name);
 
   account.save();
 
-  res.render("admin/user",{ successMessage: `${req.body.name}を削除しました` });
+  res.render("admin/user",{ 
+    successMessage: `${req.body.name}を削除しました`,
+    users: account.data
+  });
 });
 
 module.exports = router;
