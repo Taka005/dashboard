65c4581eb4934328d7d64199502ea8377b97ab60
Taka005 <01taka005@gmail.com>
2024-01-11 20:04:33 +0900

    fix

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/",(req,res)=>{
-  if(!req.session.user) return res.redirect("/account/login");
-
   res.render("index");
 });
 
